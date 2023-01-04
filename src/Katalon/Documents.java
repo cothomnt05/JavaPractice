c026e9fe636999debe8b1938663af2b090852455
Thao <cothomnt05@gmail.com>
2023-01-04 15:01:02 +0700

    Katalon_Web Browser Command

---
+++ src/Katalon/Documents.java
@@ -0,0 +1,70 @@
+package Katalon;
+
+public class Documents {
+	/*
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+'---------------------- Web Browser Command ----------------------'
+'Open browser'
+WebUI.openBrowser('');
+
+'Navigate to Url'
+WebUI.navigateToUrl('');
+
+'Back to previous page'
+WebUI.back();
+
+'Forward to next page'
+WebUI.forward();
+
+'Refresh the current page'
+WebUI.refresh();
+
+'Close window/tab by title'
+WebUI.closeWindowTitle('window/tab title');
+
+'Get current page URL'
+WebUI.getUrl();
+
+'Close window/tab by Url'
+WebUI.closeWindowUrl('window/tab Url');
+
+'Get index of current window'
+WebUI.getWindowIndex();
+
+'Close window/tab by Index'
+WebUI.closeWindowIndex('window/tab index');
+
+'Get Page Height'
+WebUI.getPageHeight();
+
+'Get Page Width'
+WebUI.getPageWidth();
+
+'Set View Port Size'
+WebUI.setViewPortSize('width', 'height');
+
+'Close browser'
+WebUI.closeBrowser();
+
+
+*/
+
+}
