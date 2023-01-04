170aecce91935173a6243e95e3eb50d4eef11c6f
Thao <cothomnt05@gmail.com>
2023-01-04 15:33:38 +0700

    Katalon_Handle User Action_Dropdown

--- src/Katalon/Documents.java
+++ src/Katalon/Documents.java
@@ -156,6 +156,92 @@ WebUI.verifyElementNotChecked(findTestObject('Radio/rad_2.0 Diese'), 10)
 "Verify radio button: 2.0 Diesel, 103kW is selected"
 WebUI.verifyElementChecked(findTestObject('Radio/rad_2.0 Diese'), 2)
 
+
+'--------- Dropdown ---------'
+'Get number of total option: Count số lượng item hiện diện trong dropdown list'
+int totalOptions = WebUI.getNumberOfTotalOption(findTestObject('Default_DropDown/Single_Dropdown'))
+println "Total items is: " + totalOptions
+
+'Verify Options Present: Kiểm tra danh sách items có hiện diện trong dropdown list hay không?'
+'Verify these text: ['Day', '1', '2', '3'] present in Date Of Birth dropdown'
+WebUI.verifyOptionsPresent(findTestObject('Default_DropDown/Single_Dropdown'), ['Day', '1', '2', '3'])
+
+'Verify Option Present By Label: Kiểm tra label (displayed text) có hiện diện trong dropdown list hay không?'
+'Verify option present by label: <option value="0">Day</option>'
+WebUI.verifyOptionPresentByLabel(findTestObject('Default_DropDown/Single_Dropdown'), 'Day', false, 30)
+
+'Verify Option Present By Value: Kiểm tra attribute value (của thẻ option) có hiện diện trong dropdown list hay không?'
+'Verify option present by value: <option value="1">1</option>'
+WebUI.verifyOptionPresentByValue(findTestObject('Default_DropDown/Single_Dropdown'), '1', false, 30)
+
+'Verify Option Not Present By Label: Kiểm tra label (displayed text) có KHÔNG hiện diện trong dropdown list hay không?'
+'Verify option not present by label: eg <option value="4">Four</option>'
+WebUI.verifyOptionNotPresentByLabel(findTestObject('Default_DropDown/Single_Dropdown'), 'Four', false, 30)
+
+'Verify Option Not Present By Value: Kiểm tra attribute value (của thẻ option) có KHÔNG hiện diện trong dropdown list hay không?'
+'Verify option not present by value: eg <option value="35">4</option>'
+WebUI.verifyOptionNotPresentByValue(findTestObject('Default_DropDown/Single_Dropdown'), '35', false, 30)
+
+'Select Option By Index: Select item được chỉ định bởi index. Trong dropdown, index bắt đầu = 0'
+'Select value on dropdown by index 31: ngay 31'
+WebUI.selectOptionByIndex(findTestObject('Default_DropDown/Single_Dropdown'), 31)
+
+'Verify Option Selected By Index: Kiểm tra item index có đang được select hay không?'
+'Verify index 31 (ngay 31) is selected'
+WebUI.verifyOptionSelectedByIndex(findTestObject('Default_DropDown/Single_Dropdown'), 31, 30)
+
+'Verify Option Not Selected By Index: Kiểm tra item index có đang KHÔNG select hay không?'
+'Verify option not selected by index 2: eg <option value="2">2</option>'
+WebUI.verifyOptionNotSelectedByIndex(findTestObject('Default_DropDown/Single_Dropdown'), 2, 30)
+
+'Select Option By Label: Select item được chỉ định bởi label (displayed text)'
+'Select value on dropdown by label: Day 10'
+WebUI.selectOptionByLabel(findTestObject('Default_DropDown/Single_Dropdown'), '10', false)
+
+'Verify Option Selected By Label: Kiểm tra label (displayed text) có đang được select hay không?'
+'Verify option Day 10 is selected'
+WebUI.verifyOptionSelectedByLabel(findTestObject('Default_DropDown/Single_Dropdown'), '10', false, 30)
+
+'Verify Option Not Selected By Label: Kiểm tra label (displayed text) có đang KHÔNG select hay không?'
+'Verify option not selected by label 20: eg <option value="20">20</option>'
+WebUI.verifyOptionNotSelectedByLabel(findTestObject('Default_DropDown/Single_Dropdown'), '20', false, 30)
+
+'Select Option By Value: Select item được chỉ định bởi attribute value (trong thẻ option).'
+'Select value on dropdown by value 15'
+WebUI.selectOptionByValue(findTestObject('Default_DropDown/Single_Dropdown'), '15', false)
+
+'Verify Option Selected By Value: Verify item được chỉ định bởi attribute value (trong thẻ option) có đang được select hay không?'
+'Verify option Day 15 is selected'
+WebUI.verifyOptionSelectedByValue(findTestObject('Default_DropDown/Single_Dropdown'), '15', false, 2)
+
+' Verify Option Not Selected By Value: Verify item được chỉ định bởi attribute value (trong thẻ option) có đang KHÔNG select hay không?'
+'Verify option not selected by value 2'
+WebUI.verifyOptionNotSelectedByValue(findTestObject('Default_DropDown/Single_Dropdown'), '2', false, 30)
+
+'Get Number Of Selected Option: Count số lượng item được select trong dropdown, Keyword này được dùng cho dropdown cho phép select nhiều item'
+'Get number of selected option'
+int selectedOptions = WebUI.getNumberOfSelectedOption(findTestObject('Default_DropDown/Multiple_Dropdown'))
+
+'Deselect All Option: Deselect tất cả các items trong dropdown, Keyword này được dùng cho dropdown cho phép select nhiều item'
+'Deselect all options'
+WebUI.deselectAllOption(findTestObject('Default_DropDown/Multiple_Dropdown'))
+
+'Select All Option: Select tất cả các items hiện diện trong dropdown, Keyword này được dùng cho dropdown cho phép select nhiều item'
+'Select all options'
+WebUI.selectAllOption(findTestObject('Default_DropDown/Multiple_Dropdown'))
+
+'Deselect Option By Index: Deselect item được chỉ định bởi index. Trong dropdown, index bắt đầu = 0.'
+'Deselect option by index 1: One'
+WebUI.deselectOptionByIndex(findTestObject('Default_DropDown/Multiple_Dropdown'), 1)
+
+'Deselect Option By Label: Deselect item được chỉ định bởi label (displayed text)'
+'Deselect option by label: Two'
+WebUI.deselectOptionByLabel(findTestObject('1.Basic/Topic_08_Handle_User_Action/05.DropdownList/Default_DropDown/Multiple_Dropdown'), "Two", false)
+
+'Deselect Option By Value: Deselect item được chỉ định bởi attribute value (trong thẻ option)'
+'Deselect option by value: 3 - Three'
+WebUI.deselectOptionByValue(findTestObject('1.Basic/Topic_08_Handle_User_Action/05.DropdownList/Default_DropDown/Multiple_Dropdown'), '3', false)
+
 */
 
 }
